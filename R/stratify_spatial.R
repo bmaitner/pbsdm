61f0ca87d0e7a7e47d4cd6b63e270451c57c9b8f
Brian <bmaitner@gmail.com>
2024-03-05 12:41:28 -0500

    updates per sf+terra transition

--- R/stratify_spatial.R
+++ R/stratify_spatial.R
@@ -2,7 +2,7 @@
 #' @description Splitting tool for cross-validation
 #' @details
 #' See Examples.
-#' @param occurrence_sp a SpatialPoints or SpatialPointsDataFrame object
+#' @param occurrence_sf a sf object containing occurrence points
 #' @param nfolds number of desired output folds. Default value of NULL makes a reasonable guess based on sample size.
 #' @param nsubclusters intermediate number of clusters randomly split into nfolds. Default value of NULL makes a reasonable guess based on sample size.
 #' If you specify this manually, it should be an integer multiple of nfolds.
@@ -11,7 +11,7 @@
 # @examples
 #' @return Returns a SpatialPoints dataframe with the data.frame containing fold designation for each point.
 #' @author Cory Merow <cory.merow@@gmail.com>
-stratify_spatial <- function(occurrence_sp,
+stratify_spatial <- function(occurrence_sf,
                             nfolds = NULL,
                             nsubclusters = NULL){
 
@@ -19,19 +19,16 @@ stratify_spatial <- function(occurrence_sp,
 
 
     #Get number of occurrence records
-    n <- length(occurrence_sp)
+    n <- nrow(occurrence_sf)
 
     #Check for sufficient records for CV
     if (n <= 5) {
-      folds <- rep(1, n)
 
-      occurrence_sp <- SpatialPointsDataFrame(coords = occurrence_sp,
-                                              data = data.frame(fold = folds))
+      occurrence_sf$fold = 1
 
       message('Get some more data if you want to do spatial CV. All samples assigned to fold 1.')
 
-      return(occurrence_sp)
-
+      return(occurrence_sf)
 
     }
 
@@ -41,43 +38,52 @@ stratify_spatial <- function(occurrence_sp,
 
       #Small number of occs
       if (n > 5 & n <= 15) {
-        folds.tmp <- kmeans(x = occurrence_sp@coords,
+
+        folds.tmp <- kmeans(x = occurrence_sf %>% st_coordinates(),
                             centers = 5)
+
         folds.tmp1 <- flexclust::as.kcca(folds.tmp,
-                                         data = occurrence_sp@coords)
-        occurrence_sp <-
-          SpatialPointsDataFrame(coords = occurrence_sp,
-                                 data = data.frame(fold = folds.tmp$cluster))
+                                         data = occurrence_sf %>% st_coordinates())
+
+        occurrence_sf$fold = folds.tmp$cluster
 
       }
 
       #Medium number
       if (n > 15 & n <= 30) {
-        folds.tmp <- kmeans(x = occurrence_sp@coords,
+
+        folds.tmp <- kmeans(x = occurrence_sf %>% st_coordinates(),
                             centers = 10)
+
         folds.tmp1 <- flexclust::as.kcca(folds.tmp,
-                                         data= occurrence_sp@coords)
+                                         data= occurrence_sf %>% st_coordinates())
+
         folds <- folds2 <- folds.tmp$clust
+
         combine.folds <- matrix(sample(x = 1:nrow(folds.tmp$centers),
                                        size =  10,
                                        replace=F),
                                 ncol = 2)
+
         for(ii in 1:nrow(combine.folds)){
           folds[folds2 %in% c(combine.folds[ii,])] <- ii
         }
 
-        occurrence_sp <- SpatialPointsDataFrame(coords = occurrence_sp,
-                                                data = data.frame(fold = folds))
+        occurrence_sf$fold = folds
 
       }
 
       #Large
       if (n > 30 & n <= 45) {
-        folds.tmp <- kmeans(x = occurrence_sp@coords,
+
+        folds.tmp <- kmeans(x = occurrence_sf %>% st_coordinates(),
                             centers = 15)
+
         folds.tmp1 <- flexclust::as.kcca(folds.tmp,
-                                         data = occurrence_sp@coords)
+                                         data = occurrence_sf %>% st_coordinates())
+
         folds <- folds2 <- folds.tmp$clust
+
         combine.folds <- matrix(sample(x = 1:nrow(folds.tmp$centers),
                                        size = 15,
                                        replace = F),
@@ -86,55 +92,69 @@ stratify_spatial <- function(occurrence_sp,
           folds[folds2 %in% c(combine.folds[ii,])] <- ii
         }
 
-        occurrence_sp <- SpatialPointsDataFrame(coords = occurrence_sp,
-                                                data = data.frame(fold = folds))
+        occurrence_sf$fold = folds
 
       }
 
       #Extra large yields 5 folds
       if (n > 45 & n <= 60) {
 
-        folds.tmp <- kmeans(x = occurrence_sp@coords,
+        folds.tmp <- kmeans(x = occurrence_sf %>% st_coordinates(),
                             centers = 20)
+
         folds.tmp1 <- flexclust::as.kcca(folds.tmp,
-                                         data = occurrence_sp@coords)
+                                         data = occurrence_sf %>% st_coordinates())
+
         folds <- folds2 <- folds.tmp$clust
+
         combine.folds <- matrix(sample(x = 1:nrow(folds.tmp$centers),
                                        size = 20,
                                        replace = F),
                                 ncol = 4)
+
         for(ii in 1:nrow(combine.folds)){
           folds[folds2 %in% c(combine.folds[ii,])] <- ii
-          }
-        occurrence_sp <- SpatialPointsDataFrame(coords = occurrence_sp,
-                                                data = data.frame(fold = folds))
+        }
+
+        occurrence_sf$fold = folds
+
         }
 
       #Truckstop soda size
+
       if (n > 60) {
-        folds.tmp <- kmeans(occurrence_sp@coords,25)
+
+        folds.tmp <- kmeans(occurrence_sf %>% st_coordinates(),25)
+
         folds.tmp1 <- flexclust::as.kcca(folds.tmp,
-                                         data = occurrence_sp@coords)
+                                         data = occurrence_sf %>% st_coordinates())
+
         folds <- folds2 <- folds.tmp$clust
+
         combine.folds <- matrix(sample(x = 1:nrow(folds.tmp$centers),
                                        size = 25,
                                        replace = F),
                                 ncol = 5)
+
         for(ii in 1:nrow(combine.folds)){
           folds[folds2%in%c(combine.folds[ii,])] <- ii
-          }
-        occurrence_sp <- SpatialPointsDataFrame(coords = occurrence_sp,
-                                                data = data.frame(fold = folds))
         }
+
+        occurrence_sf$fold = folds
+
+      }
+
     } else { # end default splitting rules
 
       if(is.null(nsubclusters)){
         stop('you must specify nsubclusters')
       }
 
-      folds.tmp <- kmeans(x = occurrence_sp@coords,
+      folds.tmp <- kmeans(x = occurrence_sf %>% st_coordinates(),
                           centers = nsubclusters)
+
       folds <- folds2 <- folds.tmp$clust
+
       combine.folds <- matrix(sample(1:nrow(folds.tmp$centers),
                                      nsubclusters, replace = F),
                               nrow = nsubclusters,
@@ -143,11 +163,11 @@ stratify_spatial <- function(occurrence_sp,
         folds[folds2 %in% c(combine.folds[ii,])] <- ii
         }
 
-      occurrence_sp <- SpatialPointsDataFrame(coords = occurrence_sp,
-                                              data = data.frame(fold = folds))
+      occurrence_sf$fold = folds
+
     }
 
-    return(occurrence_sp)
+    return(occurrence_sf)
   })
   return(out)
 }
