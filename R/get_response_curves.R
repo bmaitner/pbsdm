6b7875ad0fe88c588e4edcf5a3fc7a71fc49fd20
Brian <bmaitner@gmail.com>
2024-03-15 16:05:32 -0400

    updated code to hande density ratio models

--- R/get_response_curves.R
+++ R/get_response_curves.R
@@ -2,21 +2,14 @@
 #'
 #' Given an environmental data set, fitted models, and a directory to output plots, this function generates response curves for each predictor in the model. The response curves depict the predicted change in probability of presence as a function of the environmental predictor while holding all other predictors constant at their mean values.
 #'
-#' @param dirs A list of directories that includes the algorithm directory and diagnostics directory.
-#' @param env An \code{RasterStack} or \code{RasterBrick} of environmental predictors.
-#' @param stats A data.frame that contains information about each model that was fit, including model names, species names, and which algorithm was used.
-#' @param bg An \code{OccurrenceData} object containing the background data for each species.
-#' @param toDo A list of parameters that specifies the details of the analysis to be run.
-#' @param best.var A vector of character strings that specifies the environmental predictors to include in the response curves.
-#' @param priors A list that contains prior distributions for each parameter in the model.
-#' @param pres An \code{OccurrenceData} object containing the presence data for each species.
-#' @param envMeans A vector of means for each environmental predictor in the dataset.
-#' @param envSDs A vector of standard deviations for each environmental predictor in the dataset.
-#' @param extrapLimits A list of limits to use for extrapolation of the response curves.
-#' @param openFigs A logical value indicating whether to open the generated plots.
+#' @param env_bg Object returned by get_env_bg
+#' @param env_pres Object returned by get_env_pres
+#' @param pnp_model Object returned  by `fit_plug_and_play` or `fit_density_ratio`
+#' @param n.int Number of points along which to calculate the response curve
+#' @param envMeans A vector of means for each environmental predictor in the dataset. (not used)
+#' @param envSDs A vector of standard deviations for each environmental predictor in the dataset.(not used)
 #'
-#' @return This function generates a set of PDF files in the diagnostics directory for each species, with one file per model. The PDF files contain plots of the response curves for each environmental predictor in the model.
-#'
+#' @return This function generates a set of marginal predictions for each environmental variable, holding other variables constant
 #' @export
 #' @author Cory Merow, modified by Brian Maitner
 
@@ -97,9 +90,20 @@ get_response_curves <- function(env_bg,
     newdat[best.var[[k]]] <- x.vals[[best.var[k]]]
 
 
-    pr <- project_plug_and_play(pnp_model = pnp_model,
-                                data = newdat)
 
+      if(inherits(pnp_model,"dr_model")){
+
+        pr <- project_density_ratio(dr_model =  pnp_model,
+                                    data = newdat)
+
+      }
+
+    if(inherits(pnp_model,"pnp_model")){
+
+      pr <- project_plug_and_play(pnp_model = pnp_model,
+                                  data = newdat)
+
+    }
 
     # estimate background density at new points
       bgd_newdat <- approx(x = bgd[[best.var[k]]]$x,
@@ -122,15 +126,15 @@ get_response_curves <- function(env_bg,
                presence_density = pd_newdat$y
                ) %>%`rownames<-`(NULL)
 
-    out_k %>%
-      mutate(std_pred = prediction/sum(prediction)*sum(background_density))%>%
-      ggplot(mapping = aes(x=x_values,
-                           y=std_pred))+
-      geom_line(color="blue",size=1.5)+
-      geom_line(mapping = aes(x=x_values,y=presence_density))+
-      geom_line(mapping = aes(x=x_values,y=background_density))+
-      ylab(NULL)+
-      xlab(best.var[[k]])
+    # out_k %>%
+    #   mutate(std_pred = prediction/sum(prediction)*sum(background_density))%>%
+    #   ggplot(mapping = aes(x=x_values,
+    #                        y=std_pred))+
+    #   geom_line(color="blue",size=1.5)+
+    #   geom_line(mapping = aes(x=x_values,y=presence_density))+
+    #   geom_line(mapping = aes(x=x_values,y=background_density))+
+    #   ylab(NULL)+
+    #   xlab(best.var[[k]])
 
     marginal_vals <- marginal_vals %>%
       dplyr::bind_rows(out_k)
