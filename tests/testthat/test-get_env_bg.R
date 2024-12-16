0d020bbac156519529321a1af77fa5f93ea9322f
Brian <bmaitner@gmail.com>
2024-12-16 15:57:58 -0500

    first batch of tests working

--- tests/testthat/test-get_env_bg.R
+++ tests/testthat/test-get_env_bg.R
@@ -1,34 +1,26 @@
 context("get_env_bg")
 
-
 test_that("get_env_bg returns correct format", {
 
-   library(S4DM)
-   library(terra)
+  library(S4DM)
+  library(terra)
 
-   # occurrence points
-     data("sample_points")
-     occurrences <- sample_points
+  # occurrence points
+  data("sample_points")
+  occurrences <- sample_points
 
-   # environmental data
-     env <- rast(system.file('ex/sample_env.tif', package="S4DM"))
+  # environmental data
+  env <- rast(system.file('ex/sample_env.tif', package="S4DM"))
 
-   # rescale the environmental data
+  env <- terra::scale(env) #Note: this line causes an error without the explicit call to terra
 
-     env <- scale(env)
+  out <- get_env_bg(coords = occurrences,
+                 env = env,
+                 method = "buffer",
+                 width = 100,
+                 standardize = FALSE)
 
-  bg_data <- get_env_bg(coords = occurrences,
-                        env = env,
-                        method = "buffer",
-                        width = 100,
-                        standardize = FALSE)
+  expect_equal(object = length(out),expected = 4)
 
-  length(bg_data)
-
-
-  # test below assume a data.frame and will be skipped if one isn't returned
-  expect_equal(object = length(bg_data),expected = 4)
-  expect_contains(object = names(bg_data),
-                  expected = c("env","bg_cells","env_mean","env_sd" ))
 
 })
